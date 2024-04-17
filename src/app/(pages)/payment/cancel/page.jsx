1238781c87ccdfe0f95a397c7c1abc82a0cbcd29
Zubair Ahmad <zubairahmad@joyngroup.us>
2024-04-17 17:20:48 +0500

    add payment

--- src/app/(pages)/payment/cancel/page.jsx
+++ src/app/(pages)/payment/cancel/page.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 const page = () => {
   return (
     <div className='overflow-hidden mt-12 h-full'>
-    <section className='w-full flex min-h-[80vh] items-center justify-center flex-col bg-white mt-10 max-w-[1800px] mb-[60px]  px-[30px] lg:px-[60px] xll:px-[120px] py-[4rem] mx-auto'>
+    <section className='w-full flex min-h-[80vh] text-center items-center justify-center flex-col bg-white mt-10 max-w-[1800px] mb-[60px]  px-[30px] lg:px-[60px] xll:px-[120px] py-[4rem] mx-auto'>
 
         <h2 className='font-bold text-4xl'>Payment Failed</h2>
         <p>Oops! It seems like there was an issue with your payment. Please try again later or
           contact support.</p>
-        <Link className='text-blue-900 px-5 py-3 rounded-xl' href='/'>Back to home</Link>
+        <Link className='bg-blue-900 hover:bg-blue-700 text-white mt-8 px-5 py-3 rounded-xl' href='/'>Back to home</Link>
 
     </section>
     </div>
